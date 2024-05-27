0ea27680b1ef1474ce804283c62b48a2a2b96129
Deon Robinson <deon@appmunki.com>
2024-05-27 15:52:13 -0700

    Working web extension plugin

--- apps/sashi-extension/src/background.ts
+++ apps/sashi-extension/src/background.ts
@@ -1,8 +1,8 @@
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener((message: any) => {
   console.log('Extension Installed')
   if (message.action === 'get-config') {
     
     // Perform the necessary actions, such as waking up or handling headers
-    sendResponse({ headers: 'Example headers' });
+    //sendResponse({ headers: 'Example headers' });
   }
 });
